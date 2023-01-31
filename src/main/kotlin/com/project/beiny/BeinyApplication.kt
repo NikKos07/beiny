9f10e3aae339ea6a493ccbb43da97147670620a3
Шишлаков Никита <suser-1111@s1group.pro>
2023-01-31 12:20:17 +0300

    Настройка среды, установка spring framework

---
+++ src/main/kotlin/com/project/beiny/BeinyApplication.kt
@@ -0,0 +1,11 @@
+package com.project.beiny
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BeinyApplication
+
+fun main(args: Array<String>) {
+	runApplication<BeinyApplication>(*args)
+}
