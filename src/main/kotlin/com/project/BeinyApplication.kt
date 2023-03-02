c9b546ce2ce28b14407bd9a3e004bdd2b4a5d2b1
beer <kipa080@yahoo.com>
2023-03-02 16:14:21 +0300

    База подключена, запрос на http://192.168.7.62:8080/name Добавил возможность посылать Post запросы в БД

--- src/main/kotlin/com/project/BeinyApplication.kt
+++ src/main/kotlin/com/project/BeinyApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 
-@SpringBootApplication
+@SpringBootApplication()
 class BeinyApplication
 
 	fun main(args: Array<String>) {
